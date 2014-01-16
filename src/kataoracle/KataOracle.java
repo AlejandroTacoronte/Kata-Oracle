32c334a5bf37ad25f2928155ac836a08fc73faee
alejandro <alejandro@alejandro-PC>
2014-01-16 17:05:42 +0000

    KataOracle

---
+++ src/kataoracle/KataOracle.java
@@ -0,0 +1,27 @@
+package kataoracle;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class KataOracle {
+    private static final String URL="jdbc:oracle:thin:@localhost:1521:orcl";
+    private static final String USER="system";
+    private static final String PASSWORD="orcl";
+    
+    public static void main(String[] args) throws SQLException {
+        DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM CAMBIO_EUR_A");
+        while(resultSet.next()){
+            System.out.print(resultSet.getString("DIVISA")+" ");
+            System.out.println(resultSet.getBigDecimal("CAMBIO"));
+        }
+        connection.close();
+    }
+    
+    
+}
